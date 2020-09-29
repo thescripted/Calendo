c9bdc997f6730434457890e24132b43067a90f01
Benjamin Kinga <somethingscripted@gmail.com>
2020-09-28 22:34:14 -0500

    included typescript to structure board and card data

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,20 @@
 import React from 'react';
-import Slot from './components/Slot';
 import Row from './components/Row';
 import Card from './components/Card';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import produce from 'immer';
 
-// // TODO: Remove renderSlot.
-// function renderSlot(row_idx, col_idx, card) {
-//     function displayCard() {
-//         if (card !== undefined) {
-//             // Placeholder. Ideally, Card id is determined before it is rendered.
-//             return <Card id={card[0] * 10 + card[1]} length={card[2]} />;
-//         } else {
-//             return undefined;
-//         }
-//     }
-
-//     function doNothing() {
-//         return;
-//     }
-
-//     return (
-//         <Slot key={`${row_idx}-${col_idx}`} row={row_idx} col={col_idx} updater={doNothing}>
-//             {displayCard()}
-//         </Slot>
-//     );
-// }
-
-// TODO: How should a card data piece look like? How is it connected to the view?
-
-// Check if card is in the data layer. Returns undefined if it is not.
-function getCardFromData(row, col) {
-    const TEMPORARY_ROW = 3;
-    const TEMPORARY_COLUMN = 2;
-    const LENGTH = 1;
-    if (row === TEMPORARY_ROW && col === TEMPORARY_COLUMN) {
-        //PLACEHOLDER (!!!)
-        return [TEMPORARY_ROW, TEMPORARY_COLUMN, LENGTH];
-    } else {
-        return undefined;
-    }
-}
 const rowCoordinates = [25, 75, 125, 175, 225, 275, 325, 375, 425, 475, 525, 575]; // Perhaps kept in a constant file.
+
+interface ICard {
+    level: number;
+    height: number;
+}
+
+interface IRow {
+    cards: ICard[]
+}
 
 function comparison(coordinate) {
     let minVal = Number.MAX_SAFE_INTEGER;
@@ -58,18 +30,17 @@ function comparison(coordinate) {
 }
 
 function App() {
-    const [card, setCardState] = React.useState([[], [], [], [], []]); // # of arrays = # of rows in calendar.
-    console.log(card);
+    const [board, setBoardState] = React.useState<IRow[]>(); // # of arrays = # of rows in calendar.
 
     function generateCard(rowIndex, cardHeight) {
-        const newCard = { level: 1, height: cardHeight - 25 };
-        const nextState = produce(card, draftState => {
-            draftState[rowIndex].push(newCard);
+        const newCard: ICard = { level: 1, height: cardHeight - 25 };
+        const nextState: IRow[] = produce(board, draftState => {
+            draftState[rowIndex].cards.push(newCard);
         });
-        setCardState(nextState);
+        setBoardState(nextState);
     }
 
-    function calendarClickHandler(row_index, event) {
+    function calendarClickHandler(rowIdx: Number, event) {
         // should rewrite to calendarClickEventHandler or something like that
         event.persist();
         // Ensure that nativeEvent is only activated on the row, not any card/models/etc.
@@ -85,7 +56,7 @@ function App() {
         console.log(`x-coord: ${event.nativeEvent.offsetX}, y-coord: ${event.nativeEvent.offsetY}`);
         // event.stopPropagation();
         const { index, value } = comparison(yCoord); // Determine the height of where I need to render the card.
-        generateCard(row_index, value);
+        generateCard(rowIdx, value);
         console.log(`${yCoord} is closest to ${value}, which has index ${index}`);
     }
 
@@ -93,20 +64,9 @@ function App() {
         <div className='App'>
             <DndProvider backend={HTML5Backend}>
                 <div className='container'>
-                    {/* Remove Slots for a more generic "Row". The cards will be mapped via height & absolute positioning.
-                        {Array.from({ length: 7 }).map(function (_, row_idx) {
-                        return (
-                            <Row key={`${row_idx}`}>
-                                {Array.from({ length: 12 }).map(function (_, col_idx) {
-                                    const card = getCardFromData(row_idx, col_idx);
-                                    return renderSlot(row_idx, col_idx, card);
-                                })}
-                            </Row>
-                        );
-                    })} */}
-                    {card.map((single_row, idx) => (
+                    {board.map((single_row, idx) => (
                         <Row rowIdx={idx} key={idx} calendarClickHandler={calendarClickHandler}>
-                            {single_row.map(function (carditem) {
+                            {single_row.cards.map(function (carditem) {
                                 return (
                                     <Card
                                         content='Hello, World'
