cafdfd4ebe896999e081b664029b0833bb58ead8
Benjamin Kinga <somethingscripted@gmail.com>
2020-10-11 16:06:08 -0500

    adding navigation

---
+++ src/components/Navigation.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from "./styles/Navigation.module.css"
+
+export default function Navigation(props) {
+    return(
+        <div className={styles.navigation_wrapper}>
+            <p> Navigation</p>
+        </div>
+    )
+}
