fbfef010b77b8d875f2d00ba007afd90cbb012e8
Benjamin Kinga <somethingscripted@gmail.com>
2020-10-11 16:56:01 -0500

    added navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,8 +3,23 @@ import styles from "./styles/Navigation.module.css"
 
 export default function Navigation(props) {
     return(
+        <div className={styles.container}>
         <div className={styles.navigation_wrapper}>
-            <p> Navigation</p>
+            <div className={styles.logo}>
+                <h2>Calendo</h2>
+            </div>
+            <div className={styles.row_nav}>
+                <div className={styles.button_container}>
+                    <button id={styles.left_button}/>
+                    <button id={styles.right_button}/>
+                </div>
+                <div className={styles.month_field}>
+                    <h2>
+                        October 2020
+                    </h2>
+                </div>
+            </div>
+        </div>
         </div>
     )
 }
