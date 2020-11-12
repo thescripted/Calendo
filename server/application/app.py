18321b6b91d4e9185fa4019f2d274ececd97e2a1
Benjamin Kinga <somethingscripted@gmail.com>
2020-11-12 16:41:45 -0600

    sysadmin stuff

--- server/application/app.py
+++ server/application/app.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, request
+import uwsgi
+
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "<h1 style='color:blue'>Hello</h1>"
+    return "<h1 style='color:blue'>Hello!</h1>"
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+@app.route("/websocket")
+def ws():
+    # Complete the handshake
+    uwsgi.websocket_handshake(request.environ['HTTP_SEC_WEBSOCKET_KEY'],
+                              request.environ.get('HTTP_ORIGIN', ''))
+    # Holds the connection, and echos.
+    while True:
+        msg = uwsgi.websocket_recv()
+        print(msg)
+        uwsgi.websocket_send("Bitch")
