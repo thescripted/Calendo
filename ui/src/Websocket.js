6db85c6495686ba98d68153f929ba4184449606c
Benjamin Kinga <somethingscripted@gmail.com>
2020-11-29 13:40:22 -0600

    created websocket context

---
+++ ui/src/Websocket.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import io from 'socket.io-client'
+const DEV_URL = 'http://localhost:5000' 
+
+const WebSocketContext = React.createContext(undefined)
+
+export { WebSocketContext }
+
+export default function({ children }) {
+  let socket;
+  let ws;
+
+  function sendState(currentState, user_id, board_url) {
+    const payload = {
+      board_url: board_url,
+      user_id: user_id,
+      data: currentState,
+    }
+  }
+
+  if (!socket) {
+    socket = io.connect(DEV_URL)
+    socket.on('event://get-message', function(msg) {
+      const payload = JSON.parse(msg)
+      console.log(payload)
+    })
+
+    socket.on('connect', function() {
+      socket.emit("event://connection", "User has connected")
+
+    });
+    ws = {
+      socket: socket,
+      sendState
+    }
+  }
+  return (
+    <WebSocketContext.Provider value={ws}>
+      {children}
+    </WebSocketContext.Provider>
+  )
+}
+
