75b82969914a98cc437703293c5388f44efd6bd9
Benjamin Kinga <somethingscripted@gmail.com>
2020-11-29 21:51:10 -0600

    better deserializing in client, implemented redis

--- server/server.py
+++ server/server.py
@@ -1,25 +1,30 @@
 from flask import Flask
-from flask_socketio import SocketIO, send
+from flask_socketio import SocketIO, send, emit
+import redis
 import sys
+DEV_CLIENT_URL = 'http://localhost:3000'
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret!'
-
-
-socketio = SocketIO(app, cors_allowed_origins='http://localhost:3000')
-print(socketio, file=sys.stderr)
-
-
-@app.route("/")
-def hello():
-    print("Howdy!", file=sys.stderr)
-    return "Hello, there!"
+r = redis.Redis(host='localhost', port=6379, db=0)
+socketio = SocketIO(app, cors_allowed_origins=DEV_CLIENT_URL)
 
 
 @socketio.on("message")
-def handleMessage(msg):
-    print("Message: " + msg, file=sys.stderr)
+def handle_message(msg):
     send("Howdy!", broadcast=True)
+
+
+@socketio.on("event://calendar")
+def handle_calendar(msg):
+    r.set('default', msg)
+    emit('event://calendar', r.get('default').decode(), broadcast=True)
+
+
+@socketio.on("event://init")
+def handle_init(msg):
+    current_state = r.get('default')
+    emit('event://init', current_state.decode())
 
 
 if __name__ == "__main__":
--- ui/src/context/WebSocketContext.js
+++ ui/src/context/WebSocketContext.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import io from 'socket.io-client'
+import { StoreContext } from './StoreContext'
 const DEV_URL = 'http://localhost:5000' 
 
 const WebSocketContext = React.createContext(undefined)
 
+
+// Not ideal to have this defined here. Should move to utils.
+function dateTimeReviver(key, value) {
+  if (key === "date" || key === "startTime" || key === "endTime") {
+    return new Date(value)
+  }
+  return value
+}
+
 function WebSocketProvider(props) {
+  const { setBoardState } = React.useContext(StoreContext)
   function sendState(currentState, user_id, board_url) {
     const payload = {
       board_url: board_url,
@@ -15,15 +26,22 @@ function WebSocketProvider(props) {
   }
 
   const socket = io.connect(DEV_URL)
-  socket.on('event://calendar', function(msg) {
-    const payload = JSON.parse(msg)
+  socket.on('connect', function() {
+    socket.emit("event://init", "User has connected")
+  });
+
+  socket.on('event://init', function(msg) {
+    const payload = JSON.parse(msg, dateTimeReviver)
     console.log(payload)
+    setBoardState(payload)
   })
 
-  socket.on('connect', function() {
-    socket.emit("event://connection", "User has connected")
+  socket.on('event://calendar', function(msg) {
+    const payload = JSON.parse(msg, dateTimeReviver)
+    console.log(payload)
+    setBoardState(payload)
+  })
 
-  });
   const value = {
     socket,
     sendState
