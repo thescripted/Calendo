6c579db4a8068d54790b8d6163e1e3dd8abdff3d
Benjamin Kinga <somethingscripted@gmail.com>
2020-12-14 19:21:53 -0600

    imported RedisJSON

--- server/server.py
+++ server/server.py
@@ -2,12 +2,17 @@ from flask import Flask
 from flask_socketio import SocketIO, send, emit
 import redis
 import sys
+from rejson import Client, Path
+import json
 DEV_CLIENT_URL = 'http://localhost:3000'
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret!'
 r = redis.Redis(host='localhost', port=6379, db=0)
+rj = Client(host='localhost', port=6379, decode_responses=True)
 socketio = SocketIO(app, cors_allowed_origins=DEV_CLIENT_URL)
+jsparser = json.JSONDecoder()
+json_serializer = json.JSONEncoder()
 
 
 @socketio.on("message")
@@ -17,16 +22,19 @@ def handle_message(msg):
 
 @socketio.on("event://calendar")
 def handle_calendar(msg):
-    r.set('default', msg)
-    emit('event://calendar', r.get('default').decode(), broadcast=True)
+    json_msg = jsparser.decode(msg)
+    rj.jsonset('blank', Path.rootPath(), json_msg)
+    serialized_msg = json_serializer.encode(rj.jsonget('blank'))
+    emit('event://calendar', serialized_msg, broadcast=True)
     print("fired off an event!")
 
 
 @socketio.on("event://init")
 def handle_init(msg):
-    current_state = r.get('default')
-    if current_state is not None:
-        emit('event://init', current_state.decode())
+    json_state = rj.jsonget('blank')
+    print(json_state, file=sys.stderr)
+    if json_state is not None:
+        emit('event://init', json_serializer.encode(json_state))
         print("fired off an event!")
 
 
