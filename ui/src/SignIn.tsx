f3818c97d1597440284248e17b15077701c57b32
Benjamin Kinga <somethingscripted@gmail.com>
2020-12-16 20:37:18 -0600

    basic styling for Calendo sign-in page

--- ui/src/SignIn.tsx
+++ ui/src/SignIn.tsx
@@ -6,6 +6,7 @@ function SignIn() {
         <Layout>
             <Logo>Calendo</Logo>
             <Form>
+                <Header>Login to your Account</Header>
                 <Username />
                 <Room />
                 <SignInButton />
@@ -22,19 +23,31 @@ function Layout({ children }) {
     );
 }
 
+function Header({ children }) {
+    return (
+        <div className={styles.header}>
+            <h2>{children}</h2>
+        </div>
+    );
+}
+
 function Logo({ children }) {
     return <h1>{children}</h1>;
 }
 
 function Form({ children }) {
-    return <div className={styles.form}>{children}</div>;
+    return (
+        <form className={styles.form}>
+            <div className={styles.formcontent}>{children}</div>
+        </form>
+    );
 }
 
 function Username() {
     return (
         <div className={styles.username}>
-            <label htmlFor="username">Username:</label>
-            <input type="text" name="name" id="name" />
+            <label htmlFor="username">Username</label>
+            <input type="text" name="name" id="name" placeholder="Your Name" />
         </div>
     );
 }
@@ -42,14 +55,14 @@ function Username() {
 function Room() {
     return (
         <div className={styles.username}>
-            <label htmlFor="room">Room:</label>
-            <input type="text" name="room" id="room" />
+            <label htmlFor="room">Room</label>
+            <input type="text" name="room" id="room" placeholder="Room ID" />
         </div>
     );
 }
 
 function SignInButton() {
-    return <button>Sign In!</button>;
+    return <button>Enter Room</button>;
 }
 
 export default SignIn;
